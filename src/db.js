85e4cbe5c464f2c945fe0e24dad9d46fab7e48ff
TitoKamau053 <titusmainakamau053@gmail.com>
2025-07-24 22:14:04 +0300

    API SSL Update

--- src/db.js
+++ src/db.js
@@ -3,12 +3,12 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT, // Ensure the custom port is used (Aiven: 25705)
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: process.env.DB_SSL === 'true' ? {
-    rejectUnauthorized: true,
+    rejectUnauthorized: false, 
   } : false,
   waitForConnections: true,
   connectionLimit: 10,
